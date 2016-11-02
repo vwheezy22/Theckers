a27fb4e98ec4b79f26d7dde36b378c87312a3230
Vincent Whizin-373001110 <373001110@vusdnoc.vusd.k12.ca.us>
2016-11-02 10:25:10 -0700

    Theckers

---
+++ src/theckers/Pawn.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theckers;
+
+import java.awt.*;
+
+public class Pawn extends Piece{
+    final int HEALTH_LVL = 250;
+    final int ATTACK_LVL = 250;
+    final int RANGE_NERF = 50;
+    
+    public Pawn(Color _color)
+    {
+        super(_color);
+        setHealth();
+        setAttack();
+        setRange();
+    }
+    
+    public void setHealth()
+    {
+        health = HEALTH_LVL;
+    }
+    
+    public void setAttack()
+    {
+        attack = ATTACK_LVL;
+    }
+    
+    public void setRange()
+    {
+        range = RANGE_NERF;
+    }
+    
+    public void drawPiece(Graphics2D g, int row, int col)
+    {
+        g.fillOval(Window.getX(col*getXDelta()),Window.getY(row*getYDelta()),getXDelta(),getYDelta());
+        
+        g.setColor(Color.black);
+        g.setFont(new Font("Broadway",Font.PLAIN,20));
+        g.drawString(String.valueOf(getHealth()), Window.getX(col* getXDelta()) + getXDelta()/2, Window.getY(row * getYDelta()) + getYDelta()/2); 
+    }
+    
+}
---
+++ src/theckers/Piece.java
@@ -0,0 +1,54 @@
+package theckers;
+
+import java.awt.*;
+
+public abstract class Piece {
+    private Color color;
+    protected int health;
+    protected int attack;
+    protected int range;    //subtract a certain amount from attack for each square away when attacking
+    private int xdelta = Window.getWidth2()/Board.NUM_COLUMNS;
+    private int ydelta = Window.getHeight2()/Board.NUM_ROWS;
+    
+    
+    Piece(Color _color)
+    {
+        color = _color;
+    }
+    
+    public Color getColor()
+    {
+        return(color);
+    }
+    
+    public int getHealth()
+    {
+        return(health);
+    }
+    
+    public int getAttack()
+    {
+        return(attack);
+    }
+    
+    public int getRange()
+    {
+        return(range);
+    }
+    
+    public int getXDelta()
+    {
+        return(xdelta);
+    }
+    
+    public int getYDelta()
+    {
+        return(ydelta);
+    }
+    
+    protected abstract void setHealth();    
+    protected abstract void setAttack();    
+    protected abstract void setRange();    
+    protected abstract void drawPiece(Graphics2D g, int row, int col);
+    
+}
